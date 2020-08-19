3611ee2ba4ca6bc441a760fe621f01a6f68580a3
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-08-19 03:08:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,8 +17,8 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != phrase.upcase
-    puts "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   end
 end
